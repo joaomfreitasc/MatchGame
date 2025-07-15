37ad43b511936917c744be700520b136c808a1e1
joaom <joaom@JOO>
2025-07-15 20:24:09 -0300

    Add project files.

---
+++ MatchGame/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace MatchGame
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            SetUpGame();
+        }
+
+        private void SetUpGame()
+        {
+            List<string> animalEmoji = new List<string>()
+            {
+                "😶‍🌫️","😶‍🌫️",
+                "🤑","🤑",
+                "🎲","🎲",
+                "🤖","🤖",
+                "💩","💩",
+                "👾","👾",
+                "🙈","🙈",
+                "🦑","🦑",
+            };
+            Random random = new Random();
+
+            foreach (TextBox textBox in mainGrid.Children.OfType<TextBox>())
+            {
+                int index = random.Next(animalEmoji.Count);
+                string nextEmoji = animalEmoji[index];
+                textBox.Text = nextEmoji;
+                animalEmoji.RemoveAt(index);
+            }
+        }
+    }
+}
